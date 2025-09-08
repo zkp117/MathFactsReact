2be4bb6dd6cf3f465afb2f7b00d5407b181267b6
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-09-07 20:14:41 -0400

    pt1

--- mathfacts/src/main.jsx
+++ mathfacts/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import axios from 'axios';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+axios.defaults.withCredentials = true;
+axios.xsrfCookieName = 'csrftoken';
+axios.xsrfHeaderName = 'X-CSRFToken';
+
+ReactDOM.render(
+  <Router>
     <App />
-  </StrictMode>,
+    </Router>,
+  document.getElementById('app')
 )
